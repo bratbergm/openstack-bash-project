157b221e0be189c721d7ec23f57432333b9df4d1
ubuntu <morterb@stud.ntnu.no>
2021-02-15 19:45:43 +0000

    testing

--- scripts/AutoInstallDocker.sh
+++ scripts/AutoInstallDocker.sh
@@ -1,28 +1,33 @@
 #! /bin/bash
 
 
-sudo apt-get update
+sudo apt-get update;
+
+sleep 5;
 
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common;
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sleep 5;
 
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+
+sleep 5;
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable";
 
-sudo apt-get update
+sleep 5;
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get update; 
 
+sleep 5;
 
-# .sh
-# chmod +X
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io;
+
