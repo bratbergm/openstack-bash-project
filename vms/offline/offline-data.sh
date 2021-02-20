21ed8aae282e9d8becd66c7169b0b03cacc9997b
ubuntu <morterb@stud.ntnu.no>
2021-02-20 12:28:35 +0000

    vms

--- vms/offline/offline-data.sh
+++ vms/offline/offline-data.sh
@@ -32,7 +32,40 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sleep 1
 
 
+# Offline webside
 
-final_message: "Ferdig"
+# HTML
+printf "
+<html>
+<head>
+    <title>CyberSec4U</title>
+</head>
+<body>
+    <h1>Siden er nede for vedlikehold</h1>
+</body>
+</html>
+" > /home/ubuntu/index.html
+
+sleep 1
+
+
+# Dockerfile
+printf "
+   FROM httpd:2.4
+   COPY ./index.html /usr/local/apache2/htdocs/
+" > /home/ubuntu/Dockerfile
+
+sleep 1
+
+# Build and run
+sudo docker build -t offline:v1 /home/ubuntu/
+
+sleep 1
+
+sudo docker run -dit --name offline -p 8080:80 offline:v1
+
+sleep 1
+
+
+final_message: "Ferdig!!"
 echo "Ferdig"
-
