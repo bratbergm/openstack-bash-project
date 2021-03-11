bf6ec69f57339a18e823f694f7227e7f984240bc
ubuntu <morterb@stud.ntnu.no>
2021-03-11 16:20:02 +0000

    Auto start shutdow VMs

--- scripts/startServers.sh
+++ scripts/startServers.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+# Starts servers that are shut down
+
 for server in $(openstack server list | awk '{print $4}' | sed '1,3d' | sed '$d'); do
    if [[ $(openstack server show $server | grep OS-EXT-STS:power_state | awk '{print $4}')  = Shutdown ]]; then
       openstack server start $server
