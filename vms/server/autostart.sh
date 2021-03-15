5ea069f6a46a9b8b3c9f7cbb64f67fbc45f4706b
ubuntu <morterb@stud.ntnu.no>
2021-03-15 15:06:58 +0000

    div

--- vms/server/autostart.sh
+++ vms/server/autostart.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
-
-# Starter servere som er av
-
-var=1
-
-
-while [ $var -eq 1 ]
-do
-
 for server in $(openstack server list | awk '{print $4}' | sed '1,3d' | sed '$d'); do
    if [[ $(openstack server show $server | grep OS-EXT-STS:power_state | awk '{print $4}')  = Shutdown ]]; then
-
-#Start serveren
       openstack server start $server
-
    fi;
-done
-sleep 1800
 done
